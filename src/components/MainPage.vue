0260515995f08181e1e4be2ff0dec491415e2b78
krlrtv <corytovk@yandex.ru>
2023-03-23 12:53:20 +0300

    добавил Bootstrap и navbar

---
+++ src/components/MainPage.vue
@@ -0,0 +1,60 @@
+<template>
+  
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="primary">
+        <b-container>
+        <b-navbar-brand href="#">
+          <b-icon-arrow-up></b-icon-arrow-up>
+        </b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Рецепты</b-nav-item>
+            <b-nav-item href="#" >Disabled</b-nav-item>
+          </b-navbar-nav>
+
+        
+        </b-collapse>
+        </b-container>
+      </b-navbar>
+    </div>
+
+</template>
+
+<script>
+import {
+  BNavbar, 
+  BCollapse, 
+  BNavbarNav, 
+  // BNavItemDropdown,
+  // BDropdownItem,
+  // BNavForm,
+  // BButton,
+  // BNavbarBrand,
+  // BFormInput,
+  BContainer,
+  BIconArrowUp,
+} from "bootstrap-vue"
+
+export default {
+  name: 'MainPage',
+  components: {
+    BNavbar,
+    BCollapse, 
+    BNavbarNav, 
+    // BNavItemDropdown,
+    // BDropdownItem,
+    // /BNavForm,
+    // BButton,
+    // BNavbarBrand,
+    // BFormInput,
+    BContainer,
+    BIconArrowUp,
+  }
+}
+</script>
+
+<style>
+</style>
